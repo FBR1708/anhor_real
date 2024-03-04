b858a61e545652346ab70ef7731ea9ec0057c76a
FBR1708 <maxmudjonovvalijon@gmail.com>
2024-03-04 20:18:19 +0500

    tar fayl ozgardi

--- Token.py
+++ Token.py
@@ -1,6 +1,5 @@
 Token = '6934071728:AAFRgllllnh2Oew0d9t8aAdHEUepAFWOOAg'
 
-
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
@@ -8,12 +7,3 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 bot = Bot(token=Token)
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
-
-
-
-
-
-
-
-
-
